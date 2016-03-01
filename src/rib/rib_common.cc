41da001d47a8d9b7109b6f1c803c1a9335fe116d
Xenofon Foukas <x.foukas@sms.ed.ac.uk>
2016-03-01 13:02:14 +0000

    Added tables for TBS and MCS and fixed bug on decoding frame and subframe

--- src/rib/rib_common.cc
+++ src/rib/rib_common.cc
@@ -1,17 +1,17 @@
 #include "rib_common.h"
 
 frame_t get_frame(uint32_t sfn_sf) {
-  uint32_t frame_mask = frame_mask = !((1<<4) - 1);
-  return (sfn_sf & frame_mask) >> 4;
+  uint16_t frame_mask = ((1<<12) - 1);
+  return (sfn_sf & (frame_mask << 4)) >> 4;
 }
 
 subframe_t get_subframe(uint32_t sfn_sf) {
-  uint32_t sf_mask = !(((1<<12) - 1) << 4);
+  uint16_t sf_mask = ((1<<4) - 1);
   return (sfn_sf & sf_mask);
 }
 
 std::pair<frame_t, subframe_t> get_frame_subframe(uint32_t sfn_sf) {
-  uint32_t frame_mask = frame_mask = !((1<<4) - 1);
+  uint32_t frame_mask =  !((1<<4) - 1);
   frame_t frame = (sfn_sf & frame_mask) >> 4;
   uint32_t sf_mask = !(((1<<12) - 1) << 4);
   subframe_t subframe =  (sfn_sf & sf_mask);
