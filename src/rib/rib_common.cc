be1206756700184c714eb7c074f76c15fc16bb78
Xenofon Foukas <x.foukas@sms.ed.ac.uk>
2016-03-01 13:06:42 +0000

    Fixed bug in frame and subframe decoding in a single pair

--- src/rib/rib_common.cc
+++ src/rib/rib_common.cc
@@ -11,9 +11,9 @@ subframe_t get_subframe(uint32_t sfn_sf) {
 }
 
 std::pair<frame_t, subframe_t> get_frame_subframe(uint32_t sfn_sf) {
-  uint32_t frame_mask =  !((1<<4) - 1);
-  frame_t frame = (sfn_sf & frame_mask) >> 4;
-  uint32_t sf_mask = !(((1<<12) - 1) << 4);
+  uint32_t frame_mask =  ((1<<12) - 1);
+  frame_t frame = (sfn_sf & (frame_mask << 4)) >> 4;
+  uint32_t sf_mask = ((1<<4) - 1);
   subframe_t subframe =  (sfn_sf & sf_mask);
   return std::pair<frame_t, subframe_t>(frame, subframe);
 }
