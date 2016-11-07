c320261832eee28a4ea3d13b3fd61f6486ae3c3d
Xenofon Foukas <x.foukas@sms.ed.ac.uk>
2016-11-07 13:40:56 +0200

    Added licence to source files

--- src/app/ue_scheduling_info.cc
+++ src/app/ue_scheduling_info.cc
@@ -1,24 +1,32 @@
+/* The MIT License (MIT)
+
+   Copyright (c) 2016 Xenofon Foukas
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+   
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
 #include <algorithm>
 #include <iostream>
 
 #include "ue_scheduling_info.h"
 
 void flexran::app::scheduler::ue_scheduling_info::start_new_scheduling_round(uint16_t cell_id, std::shared_ptr<const flexran::rib::ue_mac_rib_info> ue_mac_info) {
-  // std::copy(pre_nb_rbs_required_, pre_nb_rbs_required_ + MAX_NUM_CC, nb_rbs_required_);
-  // int initial_harq = current_harq_;
-  // // Find a harq that is up to date
-  // harq_uptodate_[cell_id][ue_mac_info->get_currently_active_harq(cell_id)][0] = true;
-  // do {
-  //   current_harq_++;
-  //   current_harq_ = current_harq_ % 8;
-  //   // No harq is available at the moment
-  //   //if (initial_harq == current_harq_) {
-  //   //  std::cout << "Should not have got in here" << std::endl;
-  //   //  current_harq_ = 100;
-  //   //  break;
-  //   //}
-  // } while (!harq_uptodate_[cell_id][current_harq_][0]);
-  
   std::fill( &rballoc_sub_[0][0], &rballoc_sub_[0][0] + sizeof(rballoc_sub_) /* / sizeof(flags[0][0]) */, 0 );
   std::fill(nb_rbs_required_, nb_rbs_required_ + rib::MAX_NUM_CC, 0);
   std::fill(nb_rbs_required_remaining_, nb_rbs_required_remaining_ + rib::MAX_NUM_CC, 0);
