860ec6e57ecd6690e471d6027fc885c3e723152c
shahab SHARIAT BAGHERI <shahab.shariat@eurecom.fr>
2017-03-03 13:19:21 +0100

    RRC protobufs

--- src/app/stats_manager.cc
+++ src/app/stats_manager.cc
@@ -43,14 +43,15 @@ void flexran::app::stats::stats_manager::run_periodic_task() {
       header->set_xid(0);
 
       protocol::flex_complete_stats_request *complete_stats_request(new protocol::flex_complete_stats_request);
-      complete_stats_request->set_report_frequency(protocol::FLSRF_PERIODICAL);
-      complete_stats_request->set_sf(4);
+      complete_stats_request->set_report_frequency(protocol::FLSRF_CONTINUOUS);
+      complete_stats_request->set_sf(2);
       int ue_flags = 0;
-      ue_flags |= protocol::FLUST_PRH;                                                                            
+      ue_flags |= protocol::FLUST_PHR;                                                                            
       ue_flags |= protocol::FLUST_DL_CQI;
       ue_flags |= protocol::FLUST_RLC_BS;
       ue_flags |= protocol::FLUST_MAC_CE_BS;
       ue_flags |= protocol::FLUST_UL_CQI;
+      ue_flags |= protocol::FLUST_RRC_MEASUREMENTS;
       complete_stats_request->set_ue_report_flags(ue_flags);
       int cell_flags = 0;
       cell_flags |= protocol::FLCST_NOISE_INTERFERENCE;
@@ -86,7 +87,7 @@ std::string flexran::app::stats::stats_manager::mac_config_to_string() {
 
   std::string str;
   str += "***************\n";
-  str += "MAC statistics\n";
+  str += "UE statistics\n";
   str += "****************\n";
   str += rib_.dump_mac_stats_to_string();
 
